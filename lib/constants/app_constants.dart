0ea03ff29513677f1da43dc566a7c306d3c0e271
wathilawijesinghe <wathilawijesinghe1234@gmail.com>
2026-06-26 15:01:09 +0530

    feat: initialize AppConstants with Supabase configuration credentials

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,4 @@
+class AppConstants {
+  static const String supabaseUrl = 'https://uhliuwcpqztjlujvqax.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVobGl1d2NwcXB6dGpsdWp2cWF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1Njc0NDAsImV4cCI6MjA5MjE0MzQ0MH0.qusVnUomSERG_m3-zqqasmfR8zKoSMriICBND9zuP-Q';
+}
